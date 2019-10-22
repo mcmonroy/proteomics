09b5cd38277be2e875f29f07701117647025748d
mcmonroy <mcmonroy@up.edu.ph>
2019-10-22 23:28:53 +0800

    updated parsing and fastas

--- scripts/parse.py
+++ scripts/parse.py
@@ -1,18 +1,17 @@
 import sys
-import os
-
-fastaFile = open("../proteins/"+sys.argv[1], "r")
+fastaFile = open(sys.argv[1], "r")
 org = (sys.argv[1].split(".")[0])
 
+parsed = open((org + ".faa"), "+w")
+	
 
 seq = fastaFile.read().split("\n\n") #indiv sequences with header
 for i in range (0, len(seq)): #iterate on each sequences
 	header = seq[i].split("\n")[0]
 
-	# parsed = os.open((header.split("|")[3]+".faa"), os.O_WRONLY, 0o600)
-	parsed = open((header.split("|")[3]+".faa"), "w+")
-	parsed.write('|'.join((header.split("|"))[3:]) + "\n") #remove identifier, starts with ID in writing
-	parsed.write('\n'.join(seq[i].split("\n")[1:]) + "\n")
+	# parsed = open(("FAA/"+header.split("|")[3]+".faa"), "+w")
+	parsed.write(">" +  (header.split("|"))[3] + "\n") #remove identifier, starts with ID in writing
+	parsed.write('\n'.join(seq[i].split("\n")[1:]) + "\n\n")
 
 
 fastaFile.close()
