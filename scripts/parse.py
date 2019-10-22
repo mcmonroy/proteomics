eefd7064e2e9ef51939960f6214d489083a817ca
mcmonroy <mcmonroy@up.edu.ph>
2019-10-22 14:49:00 +0800

    fixed parsing

--- scripts/parse.py
+++ scripts/parse.py
@@ -1,18 +1,21 @@
 import sys
+import os
+
 fastaFile = open(sys.argv[1], "r")
 org = (sys.argv[1].split(".")[0])
 
-parsed = open((org + ".faa"), "+w")
+# os.system("mkdir -p "+org+" || exit 1")
+parsed = open((org + "/" + org + ".pep"), "+w")
 	
 
 seq = fastaFile.read().split("\n\n") #indiv sequences with header
-for i in range (0, len(seq)): #iterate on each sequences
+for i in range (0, len(seq)):
 	header = seq[i].split("\n")[0]
 
-	# parsed = open(("FAA/"+header.split("|")[3]+".faa"), "+w")
 	parsed.write(">" +  (header.split("|"))[3] + "\n") #remove identifier, starts with ID in writing
-	parsed.write('\n'.join(seq[i].split("\n")[1:]) + "\n\n")
+	parsed.write('\n'.join(seq[i].split("\n")[1:]) + "\n\n") #sequence
 
 
 fastaFile.close()
 parsed.close()
+
